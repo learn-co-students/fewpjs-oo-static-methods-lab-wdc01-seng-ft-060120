c9decccfeb72ed8080d22a8dac1d3399de05a241
ianmcknnn <ianmcnnn@gmail.com>
2020-07-23 16:29:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
-  //add static methods here
+
+	static capitalize(string) {
+		return string[0].toUpperCase() + string.slice(1);
+	}
+
+	static sanitize(string) {
+		return string.replace(/[^\w\d \-']+/g, "");
+	}
+
+	static titleize(string) {
+		//credit to Berel for the replace regex
+		const badWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+		let newString = string.replace(/\b(\w+)/g, (word) => {
+			if(badWords.includes(word)){
+				return word;
+			}
+			else{
+				return this.capitalize(word);
+			}
+		})
+
+		return this.capitalize(newString);
+	}
 }
