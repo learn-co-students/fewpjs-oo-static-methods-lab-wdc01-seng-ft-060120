7dfe82a4822d69526fc70c9d4b75d3f386a884fe
zato91 <zatokey@gmail.com>
2020-07-24 15:56:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 class Formatter {
   //add static methods here
+  static capitalize(str){
+    let strFirst = str[0];
+    let strfirst = str[0].toUpperCase();
+    return  str.replace(strFirst,strfirst);
+  }
+
+  static sanitize(str){
+   return  str.replace(/[^\w\d \-']+/g, "");
+  }
+
+  static titleize(str){
+    let word = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let arr = [];
+    let wordArr = str.split(" ");
+    for (let i = 0; i < wordArr.length; i++) {
+      if(i === 0) {
+        arr.push(this.capitalize(wordArr[i]));
+      } else {
+        if(word.includes(wordArr[i])){
+          arr.push(wordArr[i])
+        } else {
+          arr.push(this.capitalize(wordArr[i]))
+        }
+      }
+    }
+    return arr.join(" ");
+  }
 }
