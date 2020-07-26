140fac31354259799f08497829f27d083db0622e
andy-aguilar <aaguil3@gmail.com>
2020-07-26 13:20:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 class Formatter {
   //add static methods here
+
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+
+  static titleize(string) {
+    let wordArray = string.split(" ")
+    let firstWord = wordArray[0]
+    wordArray[0] = this.capitalize(firstWord)
+
+    const capWords =  wordArray.map(function(word) {
+      if (word != "the" && word != "a" && word != "an" && word !== "but" && word != "of" && word != "and" && word != "for" && word != "at" && word != "by" && word != "from"){
+        return Formatter.capitalize(word)
+      }
+      else{
+        return word
+      }
+    })
+
+    return capWords.join(" ")
+  }
+
 }
+
